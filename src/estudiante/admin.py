c327fc7dfa94ceb0eddcfc77b5c0e5f43b6e3456
Carhuacusma <dantemoreno2001julio@gmail.com>
2021-12-04 12:41:43 -0500

    Migrate: Estudiante y Simulacro

--- src/estudiante/admin.py
+++ src/estudiante/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Simulacro
+from .models import Estudiante
 
-admin.site.register(Simulacro)
+admin.site.register(Estudiante)
--- src/sapiens/settings.py
+++ src/sapiens/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Creadas:
+    'estudiante',
+    'simulacro',
 ]
 
 MIDDLEWARE = [
