83aa4e9ce59edf7caaaa89bbfacb4b24c90aa8fd
Carhuacusma <dantemoreno2001julio@gmail.com>
2022-01-15 12:50:56 -0500

    Create clases.py

---
+++ demo_notebook/clases.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self) -> None:
+        pass
+
+class Result:
+    def __init__(self) -> None:
+        pass
+
+    def get_Dashboard():
+        ## TO DO: 
+        return
