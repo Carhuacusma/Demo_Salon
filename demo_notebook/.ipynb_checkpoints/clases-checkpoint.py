94eb5f6a09bb000d45f8a0e516527b35dc0093c2
Carhuacusma <dantemoreno2001julio@gmail.com>
2022-01-19 13:18:13 -0500

    notebook

---
+++ demo_notebook/.ipynb_checkpoints/clases-checkpoint.py
@@ -0,0 +1,14 @@
+import datetime as dt
+
+class Result:
+    def __init__(self) -> None:
+        self.date_taken = dt.datetime.now()
+
+    def get_Dashboard():
+        ## TO DO 
+        return
+
+class User:
+    def __init__(self, username) -> None:
+        self.username = username
+        pass
