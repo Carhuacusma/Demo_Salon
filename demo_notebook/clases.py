0436dfa1db1c92d5f08fba8511f2d03ca1e0ffd3
Carhuacusma <dantemoreno2001julio@gmail.com>
2022-01-19 09:41:47 -0500

    preguntas

--- demo_notebook/clases.py
+++ demo_notebook/clases.py
@@ -1,11 +1,14 @@
-class User:
-    def __init__(self) -> None:
-        pass
+import datetime as dt
 
 class Result:
     def __init__(self) -> None:
-        pass
+        self.date_taken = dt.datetime.now()
 
     def get_Dashboard():
-        ## TO DO: 
+        ## TO DO 
         return
+
+class User:
+    def __init__(self, username) -> None:
+        self.username = username
+        pass
